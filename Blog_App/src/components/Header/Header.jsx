f3a57e260cfe5ba26941d233ad7ff2cd9d0c8c17
Rachit0919 <rachit0919@gmail.com>
2025-10-05 18:06:41 +0530

    Rename "All Posts" to "My Posts" in Header component navigation

--- Blog_App/src/components/Header/Header.jsx
+++ Blog_App/src/components/Header/Header.jsx
@@ -27,7 +27,7 @@ function Header() {
     //   active: !authStatus,
     // },
     {
-      name: "All Posts",
+      name: "My Posts",
       slug: "/all-posts",
       active: authStatus,
     },
